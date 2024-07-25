dd2e49685eeddeb463338734836de2e8aeb86c27
DRV111 <xblackcrownx@gmail.com>
2024-07-25 16:26:29 +0300

    feat: fetch data in a page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,22 @@
-import Navigation from '../components/Navigation';
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
 
-export default function Page() {
+  console.log(data);
+
   return (
     <div>
       <h1>Cabins Page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            <h3>{user.name}</h3>
+            <h4>{user.username}</h4>
+            <h3>{user.email}</h3>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
