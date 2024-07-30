0546e53a3bdedb42ce575680f82d74ad41522c45
DRV111 <xblackcrownx@gmail.com>
2024-07-30 15:23:20 +0300

    feat: add Header component

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
 
 import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -25,9 +26,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
       >
-        <header>
-          <Logo />
-        </header>
+        <Header />
         <Navigation />
         <main>{children}</main>
       </body>
